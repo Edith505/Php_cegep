44cbcd6693bcbadd91ab3e147f6d7d8d34717d7e
edih <edithrand505@gmail.com>
2025-11-13 09:43:19 -0500

    Docker

--- src/index.php
+++ src/index.php
@@ -1,2 +1,16 @@
 <?php
-echo "Hello World!";
+$salutation = "Bon après-midi";
+$message = "bienvenue";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Acceuil php</title>
+</head>
+<body>
+<h1><?php echo $salutation ?></h1>
+
+</body>
+</html>
